b6baba99af9886a1f799c9fa7ffe501b094728ef
pratikparimal <pratik.parimal@gmail.com>
2019-09-02 19:00:25 +0530

    initial commit

---
+++ dictionary-serverside-api/src/main/java/com/webapp/dictionaryserversideapi/DictionaryServersideApiApplication.java
@@ -0,0 +1,13 @@
+package com.webapp.dictionaryserversideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DictionaryServersideApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DictionaryServersideApiApplication.class, args);
+	}
+
+}
---
+++ dictionary-serverside-api/src/main/java/com/webapp/dictionaryserversideapi/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.webapp.dictionaryserversideapi;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DictionaryServersideApiApplication.class);
+	}
+
+}
